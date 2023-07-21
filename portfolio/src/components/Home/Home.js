aa40833d650e4e11255214304bf8ef22e90c3710
Ariel <salinas.ariel02@gmail.com>
2023-07-21 14:54:05 -0300

    home v1
    finish homepage and navbar, no routing

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -4,8 +4,9 @@ import '../Home/Home.css'
 function home() {
   return (
     <div >
+      <div className=' scale-up-center onbody'>
         <div className='present '>
-            <h1>Hi! My name is Ariel!</h1>
+            <h1>Hi! 👋<br></br> My name is Ariel!</h1>
             <h3>I'm Software Developer</h3>
         </div>
         <div className='aboutme'>
@@ -13,7 +14,7 @@ function home() {
           Since I was little, I have been fascinated by technology and its ways of using it. Today,
           I am fulfilling my dream of learning how to use it to build new tools that will someday help the community.</p>
         </div>
-        
+      </div>
     </div>
   )
 }
